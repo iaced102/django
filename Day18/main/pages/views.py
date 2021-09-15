204ee99186a5d1dcfba896eeccd5fa226170ca24
iaced102 <lightvhp@gmail.com>
2021-09-15 15:14:40 +0700

    create app

---
+++ Day18/main/pages/views.py
@@ -0,0 +1,11 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+from django.http import HttpRequest
+
+# Create your views here.
+
+def home(request):
+    return HttpResponse('phuc deeptry')
+
+def about(request):
+    return HttpResponse('phuc cuc ki dep trai')
